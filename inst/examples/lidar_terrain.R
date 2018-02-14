c81422de4a7efde977bc2a2fef9505d4e6f564ba
Michael Sumner <Michael.Sumner@aad.gov.au>
2018-02-14 14:09:47 +1100

    gl

--- inst/examples/lidar_terrain.R
+++ inst/examples/lidar_terrain.R
@@ -39,3 +39,20 @@ tr <- triangulate(pslg(P = coords[, 1:2], PA = coords[, 3, drop = FALSE]))
 rgl.triangles(cbind(tr$P, tr$PA)[t(tr$T), ])
 
 saveRDS(tr, file = "inst/examples/lidar_terrain.rds", compress = "bzip2")
+x <- (raadfiles::thelist_files(pattern = "parcels_hobart") %>% purrr::transpose() %>% purrr::map(~sf::read_sf(.x$fullname)) )[[1]]
+
+cad <- selectFeatures(x)
+cad <- sf::st_transform(cad, "+proj=utm +zone=55 +south +datum=WGS84")
+library(pfft)
+ptm <- path_triangle_map(silicate::PATH(cad), tr)
+cols <- rep("#000000", nrow(tr$T))
+cols[ptm$triangle_idx] <- viridis::viridis(length(unique(ptm$path_)))[factor(ptm$path_)]
+rgl.triangles(cbind(tr$P, tr$PA)[t(tr$T), ], col = rep(cols, each = 3))
+pt <- rgdal::project(cbind(147.2641, -42.92700), "+proj=utm +zone=55 +south +datum=WGS84")
+ln <- cbind(rbind(pt, pt), c(300, 400))
+lines3d(ln)
+
+saveRDS(cols, file = "inst/examples/lidar_terrain_cols.rds")
+saveRDS(ln, file = "inst/examples/lidar_line.rds")
+library(anglr)
+cad <- anglr(x)
